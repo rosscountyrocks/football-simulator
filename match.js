1964e7b79f2d540ef9d1041e7be7ed198e7eaeef
Lawrence Berry <lawrence.berry@gmail.com>
2014-11-29 17:26:45 +0000

    Added Game feedback

--- match.js
+++ match.js
@@ -1,24 +1,41 @@
 var maxscore = 4;
 
 var playMatch = function(teamone, teamtwo) {
-    var scoreone;
+  var scoreone;
   var scoretwo;
+
+  ClearFeedback();
 
   scoreone = generateScore();
   scoretwo = generateScore();
 
-  console.log(teamone + " vs. " + teamtwo);
-  console.log(teamone + " " + scoreone + " - " + teamtwo + " " + scoretwo)
+  feedback(teamone + " vs. " + teamtwo);
+  feedback(teamone + " " + scoreone + " - " + teamtwo + " " + scoretwo)
 
   if (scoreone > scoretwo) {
-    console.log(teamone + " won!");
+    feedback(teamone + " won!");
   } else if (scoreone == scoretwo) {
-    console.log("its a draw!")
+    feedback("its a draw!")
   } else {
-    console.log(teamtwo + " won!");
+    feedback(teamtwo + " won!");
   }
 }
 
 var generateScore = function() {
   return Math.round(Math.random() * maxscore);
 }
+
+var feedback = function(txt) {
+  $("#feedback-content").append(txt + "<br />");
+}
+
+var ClearFeedback = function() {
+  $("#feedback-content").empty();
+}
+
+$( document ).ready(function () {
+
+  $("#play-button").on("click" , function() {
+    playMatch($("#home-team").val(), $("#away-team").val())
+  });
+});
